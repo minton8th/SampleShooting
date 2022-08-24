90be8831bdd7c346dff91a89a112a94fb38151c9
y-komoda99 <y.komoda99@mail.benesse.co.jp>
2022-08-24 15:38:00 +0900

    第5回

--- SampleShooting/Assets/Scripts/BulletController.cs
+++ SampleShooting/Assets/Scripts/BulletController.cs
@@ -9,7 +9,7 @@ public class BulletController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -17,14 +17,20 @@ public class BulletController : MonoBehaviour
     {
         transform.Translate(0, 0.2f, 0);
 
-        if(transform.position.y > 5){
+        if (transform.position.y > 5)
+        {
             Destroy(gameObject);
         }
     }
 
-    void OnTriggerEnter2D(Collider2D coll) {
-        Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        GameObject.Find("Canvas").GetComponent<UIController>().AddScore();
+
+        GameObject effect = Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+        Destroy(effect, 1.0f);
+
         Destroy(coll.gameObject);
-        Destroy(gameObject);       
+        Destroy(gameObject);
     }
 }
