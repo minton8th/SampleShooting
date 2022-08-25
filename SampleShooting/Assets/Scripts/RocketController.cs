5b5f582b898334bc11d263ca1377e5244155d117
y-komoda99 <y.komoda99@mail.benesse.co.jp>
2022-08-25 10:44:18 +0900

    移動制限を更新

--- SampleShooting/Assets/Scripts/RocketController.cs
+++ SampleShooting/Assets/Scripts/RocketController.cs
@@ -6,10 +6,22 @@ public class RocketController : MonoBehaviour
 {
     public GameObject bulletPrefab;
 
+    // 映っている画面の端のワールド座標
+    Vector2 min;
+    Vector2 max;
+
+    // 自身の幅
+    float width;
+
     // Start is called before the first frame update
     void Start()
     {
+        // 画面の左下と右上の座標を取得
+        min = Camera.main.ViewportToWorldPoint(Vector2.zero);
+        max = Camera.main.ViewportToWorldPoint(Vector2.one);
 
+        // 自身のスプライトの幅を取得
+        width = gameObject.GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
     // Update is called once per frame
@@ -17,14 +29,14 @@ public class RocketController : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            if (transform.position.x > -2.2)
+            if (transform.position.x > min.x + width / 2)
             {
                 transform.Translate(-0.1f, 0, 0);
             }
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            if (transform.position.x < 2.2)
+            if (transform.position.x < max.x - width / 2)
             {
                 transform.Translate(0.1f, 0, 0);
             }
