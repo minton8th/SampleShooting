ae06ab8aa5033daa25b13bd99dfc30dd0fecb2e3
y-komoda99 <y.komoda99@mail.benesse.co.jp>
2022-08-25 10:27:53 +0900

    横の移動制限を追加

--- SampleShooting/Assets/Scripts/RocketController.cs
+++ SampleShooting/Assets/Scripts/RocketController.cs
@@ -9,19 +9,29 @@ public class RocketController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.LeftArrow)){
-            transform.Translate(-0.1f, 0, 0);
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            if (transform.position.x > -2.2)
+            {
+                transform.Translate(-0.1f, 0, 0);
+            }
         }
-        if(Input.GetKey(KeyCode.RightArrow)){
-            transform.Translate(0.1f, 0, 0);
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            if (transform.position.x < 2.2)
+            {
+                transform.Translate(0.1f, 0, 0);
+            }
+
         }
-        if(Input.GetKeyDown(KeyCode.Space)){
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
             Instantiate(bulletPrefab, transform.position, Quaternion.identity);
         }
     }
